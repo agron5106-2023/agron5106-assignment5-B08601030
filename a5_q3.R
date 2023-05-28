043dba5aa919eb0a037130817fb3450030a2a110
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-28 22:09:41 +0800

    Add 4 test_that funct and check&trycatch to identify error
    4 test for valid input(numeric num),valid type(list),zero division,missing calues errors

--- a5_q3.R
+++ a5_q3.R
@@ -1,9 +1,51 @@
 a5_q3 <- function(input) {
-  x<-input$x
-  y<-input$y
-  answer <- list("sum"=round(x+y,2),
-                 "difference"=round(x-y,2),
-                 "product"=round(x*y,2),
-                 "ratio"=round(x/y,2))
-  return(answer)
+  check_invalid_input <- function(input) {
+    if (!is.numeric(input)) {
+      stop("Invalid input: Input should be a numeric number.")
+    }
+  }
+  
+  check_invalid_input_type <- function(input) {
+    if (!is.vector(input)) {
+      stop("Invalid input type: Input should be a vector.")
+    }
+  }
+  
+  check_zero_division <- function(x5) {
+    if (x5 == 0) {
+      stop("Zero division error: Division by zero is not allowed.")
+    }
+  }
+  
+  check_missing_value <- function(input) {
+    if (any(is.na(input))) {
+      stop("Missing value error: Input contains missing values.")
+    }
+  }
+  
+  tryCatch(
+    {
+      check_invalid_input(input$x)
+      check_invalid_input(input$y)
+      check_invalid_input_type(input)
+      check_zero_division(input$y)
+      check_missing_value(input$x)
+      check_missing_value(input$y)
+      
+      x <- input$x
+      y <- input$y
+      
+      answer <- list(
+        sum = round(x + y, 2),
+        difference = round(x - y, 2),
+        product = round(x * y, 2),
+        ratio = round(x / y, 2)
+      )
+      
+      return(answer)
+    },
+    error = function(e) {
+      print(paste(e))
+    }
+  )
 }
--- test_a5_q3_ext.R
+++ test_a5_q3_ext.R
@@ -2,8 +2,30 @@ library(testthat)
 
 source("a5_q3.R")
 
-test_that("", {
-    object <- NA
-    expected <- 0
-    expect_equal(object, expected)
+test_that("test_invalid_input", {
+  input <- list(x = "invalid", y = 2)
+  expected <- "Error in check_invalid_input(input$x): Invalid input: Input should be a numeric number.\n"
+  result <- a5_q3(input)
+  expect_equal(result, expected)
+})
+
+test_that("test_invalid_input_type", {
+  input <- c(1, 2, 3)
+  expected <- "Error in check_invalid_input_type(input): Invalid input type: Input should be a list.\n"
+  result <- a5_q3(input)
+  expect_equal(result, expected)
+})
+
+test_that("test_zero_division", {
+  input <- list(x = 5, y = 0)
+  expected <- "Error in check_zero_division(input$y): Zero division error: Division by zero is not allowed.\n"
+  result <- a5_q3(input)
+  expect_equal(result, expected)
+})
+
+test_that("test_missing_value", {
+  input <- list(x = 5, y = NA)
+  expected <- "Error in check_missing_value(input$y): Missing value error: Input contains missing values.\n"
+  result <- a5_q3(input)
+  expect_equal(result, expected)
 })
