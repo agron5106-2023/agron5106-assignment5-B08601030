64afbde62f5b81ed1d6e8a50d2ce4a7f518e67f0
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-28 17:12:46 +0800

    Add a trycatch to check the input is num or not
    using:create a funct to condition under all input is numeric num
    	if not create an error message
    THEN,using trycatch to catch the error and print out error message

--- test_a5_q2_ext.R
+++ test_a5_q2_ext.R
@@ -2,8 +2,27 @@ library(testthat)
 
 source("a5_q2.R")
 
-test_that("", {
-    object <- NA
-    expected <- 0
-    expect_equal(object, expected)
-})
+check_unvalid_input <- function(input) {
+  if (any(!is.numeric(input))) {
+    stop("Invalid input: Input should contain only numeric values.")
+  }
+}
+
+input1=c("a","b","c","d","e")
+
+tryCatch(
+  check_unvalid_input(input1),
+  error = function(e) {
+    print("====Invalid input====")
+    print(paste(e))
+  }
+)
+
+
+
+
+
+
+
+    
+
