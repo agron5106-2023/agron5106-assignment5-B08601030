139db21eff5bf25860b0de3314c6393048ff3851
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-28 11:25:04 +0800

    Modify the syntax
    there are one more ) after round(x4 / x5, 2)

--- a5_q2.R
+++ a5_q2.R
@@ -2,20 +2,21 @@
 a5_q2 <- function(input) {
   tryCatch(
     {
-      x1=input[[1]]
-      x2=input[[2]]
-      x3=input[[3]]
-      x4=input[[4]]
-      x5=input[[5]]
-      answer <- c(round(x1 + x2,2),
-                  round(x2 - x3,2),
-                  round(x3 * x4,2),
-                  round(x4 / x5),2)
+      x1 <- input[[1]]
+      x2 <- input[[2]]
+      x3 <- input[[3]]
+      x4 <- input[[4]]
+      x5 <- input[[5]]
+      answer <- c(round(x1 + x2, 2),
+                  round(x2 - x3, 2),
+                  round(x3 * x4, 2),
+                  round(x4 / x5, 2))
       return(answer)
     },
     error = function(e) {
-      return(NA)  
+      return(NA)
     }
   )
 }
+
 
