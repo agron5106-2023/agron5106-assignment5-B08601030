68e73c324bc32c106b8a0aab06cc6318f10e21ab
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-29 17:33:22 +0800

    Add 4 ext tests for a5_q4
    tests for invalid input type(not vector), invalid input(non numeric)
    missing values and insufficient input length
    
    input metrix to test input type, input string to test invalid input
    input NA to test missing values input length 1 input to test insufficient length

--- test_a5_q4_ext.R
+++ test_a5_q4_ext.R
@@ -1,9 +1,30 @@
+rm(list = ls())
+
 library(testthat)
 
 source("a5_q4.R")
 
-test_that("", {
-    object <- NA
-    expected <- 0
-    expect_equal(object, expected)
+test_that("test_invalid_input_type", {
+  input <- matrix(1, 2, 3)
+  expected <- "Invalid input type: Input should be a vector."
+  expect_error(a5_q4(input), expected)
 })
+
+test_that("test_missing_value", {
+  input <- c(1, 2, NA, 4, 5)
+  expected <-"Missing value error: Input contains missing values."
+  expect_error(a5_q4(input), expected)
+})
+
+test_that("test_invalid_input", {
+  input <- c(1, 2, "3", 4, 5)
+  expected <- "Invalid input: Input should contain only numeric values."
+  expect_error(a5_q4(input), expected)
+})
+
+test_that("test_insufficient_elements", {
+  input <- c(1)
+  expected <- "Insufficient elements: Input vector does not have enough elements for calculation."
+  expect_error(a5_q4(input), expected)
+})
+
