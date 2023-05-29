271fce85933d76b0d4a274acd31e1b2568b029ab
Tang Xin Yee <tangxinyee1107@gmail.com>
2023-05-29 16:36:37 +0800

    Add 5 tests for a5_q4
    test when the input is int, float, negative num, mixed num and large num

--- test_a5_q4_basic.R
+++ test_a5_q4_basic.R
@@ -2,8 +2,33 @@ library(testthat)
 
 source("a5_q4.R")
 
-test_that("", {
-    object <- NA
-    expected <- 0
-    expect_equal(object, expected)
+test_that("test_int", {
+  numbers <- c(1, 2, 3, 4, 5)
+  result <- a5_q4(numbers)
+  expect_equal(result, 15)
 })
+
+test_that("test_float", {
+  numbers <- c(1.5, 2.5, 3.5, 4.5, 5.5)
+  result <- a5_q4(numbers)
+  expect_equal(result, 17.5)
+})
+
+test_that("test_negative_num", {
+  numbers <- c(-1, -2, -3, -4, -5)
+  result <- a5_q4(numbers)
+  expect_equal(result, -15)
+})
+
+test_that("test_mixed_num", {
+  numbers <- c(1, 2.5, 3, 4.5, 5)
+  result <- a5_q4(numbers)
+  expect_equal(result, 16)
+})
+
+test_that("test_large_num", {
+  numbers <- c(1e6, 2e6, 3e6, 4e6, 5e6)
+  result <- a5_q4(numbers)
+  expect_equal(result, 15000000)
+})
+
